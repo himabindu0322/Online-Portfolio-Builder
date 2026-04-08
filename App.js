692e4e5752aae52932ca9b4558248835e762e029
Himabindu <karrihimabindu22@gmail.com>
2026-04-08 09:29:44 +0530

    Initialize React Router in App component
    Set up routing for Register, Login, Dashboard, and PortfolioForm pages.

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import PortfolioForm from "./pages/PortfolioForm";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-portfolio" element={<PortfolioForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
